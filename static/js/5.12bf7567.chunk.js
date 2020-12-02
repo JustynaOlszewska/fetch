1531f6dfbc687bb640c747f25f6235023b808798
Justyna Olszewka <wioza@wp.pl>
2020-12-02 10:13:55 +0100

    Updates

--- static/js/5.12bf7567.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonplisfetch=this.webpackJsonplisfetch||[]).push([[5],{37:function(t,e,n){"use strict";n.r(e);var c=n(13),s=n(1),i=n(0),r=n.n(i),o=n(12),a=n(18),b=r.a.memo((function(t){var e=t.setBooks,n=Object(c.a)(o.b,2)[1];return Object(s.jsxs)(s.Fragment,{children:[Object(s.jsx)("button",{onClick:function(){Object(a.a)(n,e,Object(o.a)("DELETE"))},children:"delete everything and write your own list"}),Object(s.jsx)("hr",{})]})}));e.default=b}}]);
-//# sourceMappingURL=5.12bf7567.chunk.js.map
